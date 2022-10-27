d50d8b01776488350798e54eb66d9aea8342d320
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-10-27 11:54:50 -0300

    configurado na app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    }
+  }
+
+  modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+  }
+
+  formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
+
+  render() {
+    return (
+      <div className="text-editor container">
+        <ReactQuill theme="snow"
+                    modules={this.modules}
+                    formats={this.formats}>
+        </ReactQuill>
+      </div>
+    );
+  }
 }
 
 export default App;
